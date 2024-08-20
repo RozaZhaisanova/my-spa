c7a54e7378497c5916824dee7665c5f2d9730080
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2024-08-21 00:40:41 +0300

    ограничение длины email, добавление иконки лайка(состояние нужно перенести со списка в карточку)

--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetUsersQuery } from "./store/apiSlice";
+import { FaHeart } from "react-icons/fa";
 
 const UserList: React.FC = () => {
   const { data, error, isLoading } = useGetUsersQuery();
-
+  const [liked, setLiked] = useState(false);
+  const toggleLike = () => {
+    setLiked((prev) => !prev);
+  };
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error fetching users.</div>;
 
@@ -17,7 +21,14 @@ const UserList: React.FC = () => {
             <p>
               {user.name.first} ${user.name.last}
             </p>
-            <p>{user.email}</p>
+            <p>
+              {user.email.length > 15
+                ? `${user.email.slice(0, 15)}...`
+                : user.email}
+            </p>
+            <button type="button" onClick={toggleLike}>
+              <FaHeart color={liked ? "red" : "gray"} size={24} />
+            </button>
           </li>
         ))}
       </ul>
